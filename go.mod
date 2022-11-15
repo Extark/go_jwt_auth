7f1e32a40074690189bd91b9e0b07c46c4b0f8f2
Elias Geir Della Schiava <eliasgeir.dellaschiava@extark.com>
2022-11-15 10:30:43 +0100

    Initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/extark/go_jwt_auth
+
+go 1.19
+
+require github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
---
+++ utils/auth.go
@@ -0,0 +1,43 @@
+package utils
+
+import (
+	"fmt"
+	"github.com/dgrijalva/jwt-go"
+	"time"
+)
+
+// generateToken creates a new token and save inside it all the data you need (es. uuid of the user)
+// data is an any type, and represents the data to save inside the token
+// tokenExpireTimeHours is a int type, represents the duration in hours of the token
+// encryptionKey is a string type, represents a secret value to encrypt the token
+func generateToken(data any, tokenExpireTimeHours int, encryptionKey string, isRefreshToken bool) (string, error) {
+	//Generate claim struct and populate it
+	claims := jwt.MapClaims{}
+
+	if !isRefreshToken {
+		claims["authorized"] = true
+	}
+	claims["data"] = data
+	claims["expire"] = time.Now().Add(time.Hour * time.Duration(tokenExpireTimeHours)).Unix()
+
+	//Generate token
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	return token.SignedString([]byte(encryptionKey))
+}
+
+// isTokenValid check if the passed token is valid
+// token is a string type and represents the token assigned to the user
+// encryptionKey is a string type, represents a secret value to encrypt the token
+func isTokenValid(token string, encryptionKey string) error {
+	_, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return []byte(encryptionKey), nil
+	})
+	if err != nil {
+		return err
+	}
+	return nil
+}
