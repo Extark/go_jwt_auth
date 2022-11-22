cf4309b3c9fa760d5153343439089e7340c8aaf8
Elias Geir Della Schiava <eliasgeir.dellaschiava@extark.com>
2022-11-22 15:51:49 +0100

    - removed old method + new method to extract token data

--- login.go
+++ login.go
@@ -1,5 +1,7 @@
 package go_jwt_auth
 
+import "net/http"
+
 // CreateTokens this method creates an access token and a refresh token and return it. Is important call this method only after the check of user and password.
 // data is an any type, and represents the data to save inside the token
 // tokenExpireTimeHours is a int type, represents the duration in hours of the token
@@ -21,5 +23,11 @@ func CreateTokens(data any, tokenExpireTimeHours int, encryptionKey string) (acc
 // GetTokenData this method gets the value of the data inside the JWT token and gets a response only if the token is valid
 func GetTokenData(token string, encryptionKey string) (data any, err error) {
 	//Check if is valid and in this case it returns the data string
+	return isTokenValid(token, encryptionKey)
+}
+
+// ExtractAndGetTokenData s
+func ExtractAndGetTokenData(r *http.Request, encryptionKey string) (data any, err error) {
+	token := extractToken(r)
 	return isTokenValid(token, encryptionKey)
 }
--- main.go
+++ main.go
@@ -3,7 +3,3 @@ package go_jwt_auth
 type StandardError struct {
 	Error string `json:"error"`
 }
-
-type Auth interface {
-	CreateTokens() (access string, refresh string, err error)
-}
